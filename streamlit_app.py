d8fc07edfcd0b5a7b933b9cf47cf355a4c8509dd
Ampet123 <jan.peter.f.virtucio@accenture.com>
2024-10-17 13:46:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
+
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5)
 if ingredients_list:
     #st.write(ingredients_list)
@@ -43,6 +46,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+
 #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=true)
